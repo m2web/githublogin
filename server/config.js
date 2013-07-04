1438194facc88cb44630153fbeb3a36c2cd5fd27
mark <m2web@yahoo.com>
2013-07-04 11:23:59 -0400

    added Meteor.settings file and .gitignore

--- server/config.js
+++ server/config.js
@@ -4,6 +4,8 @@ Accounts.loginServiceConfiguration.remove({
 
 Accounts.loginServiceConfiguration.insert({
 		service: "github",
-	clientId: "95113b0120e584254200",
-	secret: "9906710b2836c3beb35b782e9c7687deeb2abc8f"
+		//clientId: "95113b0120e584254200",
+		clientId: Meteor.settings.githubClientID,
+		//secret: "9906710b2836c3beb35b782e9c7687deeb2abc8f"
+		secret: Meteor.settings.githubSecret
 });
