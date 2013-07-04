f031941a74ebfb485e4435e613649af1e57cbf7b
mark <m2web@yahoo.com>
2013-07-03 23:53:36 -0400

    added CSS and as well as the bootstrap package

---
+++ server/config.js
@@ -0,0 +1,9 @@
+Accounts.loginServiceConfiguration.remove({
+		service: "github"
+});
+
+Accounts.loginServiceConfiguration.insert({
+		service: "github",
+	clientId: "95113b0120e584254200",
+	secret: "9906710b2836c3beb35b782e9c7687deeb2abc8f"
+});
